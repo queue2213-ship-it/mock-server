4fcab37c63e231d4ea014cd0d3ff5220debc2b6b
LnwDev <you@example.com>
2026-06-06 18:52:27 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// หน้าแรก
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// LOGIN API
+app.post("/Lnw/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const mockUser = {
+    username: "Q",
+    password: "9"
+  };
+
+  if (username === mockUser.username && password === mockUser.password) {
+    return res.json({
+      success: true,
+      token: "mock-token-abc123",
+      message: "Login success"
+    });
+  }
+
+  res.status(401).json({
+    success: false,
+    message: "Invalid username or password"
+  });
+});
+
+// START SERVER
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
